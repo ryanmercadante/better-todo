e9fa25af14f1ccea125ca246fb113a84d2246e26
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2019-02-24 14:06:06 -0500

    load todosData into state so we can change it in the future

--- src/App.js
+++ src/App.js
@@ -5,10 +5,17 @@ import todosData from "./todosData";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: todosData
+    };
+  }
   render() {
-    const todos = todosData.map(item => <TodoItem key={item.id} todo={item} />);
+    const { todos } = this.state;
+    const todoItems = todos.map(item => <TodoItem key={item.id} todo={item} />);
 
-    return <div className="todo-list">{todos}</div>;
+    return <div className="todo-list">{todoItems}</div>;
   }
 }
 
